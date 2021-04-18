5f24c68eb08b1f6857538601ea1011d238851b1c
Helgi Halldórsson <helgihalldorsson94@gmail.com>
2021-04-18 21:04:04 +0000

    Added options to delete output files

--- src/template.compiler/TemplateCompiler.cs
+++ src/template.compiler/TemplateCompiler.cs
@@ -10,52 +10,113 @@ namespace template.compiler
 {
     public class TemplateCompiler
     {
-        public static void Compile(string templateDir, string rootFile, string outputDir, string dataFilePath)
+        #region Properties
+        public TemplateData Data { get; set; }
+
+        public bool CleanUpAuxiliary { get; set; } = false;
+        public bool CleanUpTemplate { get; set; } = false;
+        public bool CleanUpSyncTex { get; set; } = false;
+
+        private readonly DirectoryInfo templateDir;
+        private readonly string rootFile;
+
+        
+        private readonly string templateDirectory = "template";
+        // Auxiliary and output moved up a level, to make all 3 directories appear in the same directory.
+        private readonly string auxiliaryDirectory = @"..\auxiliary";
+        private readonly string outputDirectory = @"..";
+        #endregion
+
+        #region Constructors
+        public TemplateCompiler(string templateDir, string rootFile, string dataFilePath, bool cleanUpAuxiliary = false, bool cleanUpTemplate = false, bool cleanUpSyncTex = false)
         {
-            if(!File.Exists(dataFilePath))
+            this.templateDir = GetDirectoryInfo(templateDir);
+            this.rootFile = rootFile;
+            if (!File.Exists(dataFilePath))
             {
                 throw new FileNotFoundException($"File {dataFilePath} not found.");
             }
-            string templateData = File.ReadAllText(dataFilePath);
-            TemplateData data = TemplateData.Deserialize(templateData);
-            Compile(templateDir, rootFile, outputDir, data);
+
+            string templateData = File.ReadAllText(dataFilePath);            
+            Data = TemplateData.Deserialize(templateData);
+
+            CleanUpAuxiliary = cleanUpAuxiliary;
+            CleanUpTemplate = cleanUpTemplate;
+            CleanUpSyncTex = cleanUpSyncTex;
         }
 
-        public static void Compile(string templateDir, string rootFile, string outputDir, TemplateData templateData)
+        public TemplateCompiler(string templateDir, string rootFile, TemplateData templateData, bool cleanUpAuxiliary = false, bool cleanUpTemplate = false, bool cleanUpSyncTex = false)
         {
-            DirectoryInfo templateDirInfo = GetDirectoryInfo(templateDir);
-            DirectoryInfo outputDirInfo = GetDirectoryInfo(outputDir);
+            this.templateDir = GetDirectoryInfo(templateDir);
+            this.rootFile = rootFile;
 
-            List<string> supportedLanguages = templateData.settings.supportedLanguages;
-            if (supportedLanguages != null && supportedLanguages.Count > 0)
-            {
-                foreach(string lang in supportedLanguages)
-                {
-                    DirectoryInfo languageDir = outputDirInfo.CreateSubdirectory(lang);
-                    CompileSingleLanguage(templateDirInfo, rootFile, languageDir, templateData, lang);
-                }
-            }
-            else
-            {
-                CompileSingleLanguage(templateDirInfo, rootFile, outputDirInfo, templateData);
-            }
-        }
+            Data = templateData;
 
-        private static void CompileSingleLanguage(DirectoryInfo templateDirInfo, string rootFile, DirectoryInfo outputDirInfo, TemplateData templateData, string selectedLanguage = null)
-        {
-            DirectoryInfo outputTemplateDirInfo = outputDirInfo.CreateSubdirectory("template");
-            PrepareTemplateDirectory(templateDirInfo, outputTemplateDirInfo, templateData, selectedLanguage);
-            LatexCompiler.Compile(outputTemplateDirInfo.FullName, rootFile, auxDir: @"..\auxiliary", outputDir: @"..");
+            CleanUpAuxiliary = cleanUpAuxiliary;
+            CleanUpTemplate = cleanUpTemplate;
+            CleanUpSyncTex = cleanUpSyncTex;
         }
+        #endregion
 
         private static DirectoryInfo GetDirectoryInfo(string dir)
         {
             DirectoryInfo dirInfo = new DirectoryInfo(dir);
             if (!dirInfo.Exists)
             {
-                throw new DirectoryNotFoundException($"Directory {dir} not found.");
+                throw new DirectoryNotFoundException($"Directory {dirInfo.FullName} not found.");
             }
             return dirInfo;
+        }
+
+        public void Compile(string outputDir)
+        {
+            DirectoryInfo outputDirInfo = new DirectoryInfo(outputDir);
+            if(!outputDirInfo.Exists)
+            {
+                outputDirInfo.Create();
+            }
+
+            List<string> supportedLanguages = Data.settings.supportedLanguages;
+            if (supportedLanguages != null && supportedLanguages.Count > 0)
+            {
+                foreach(string lang in supportedLanguages)
+                {
+                    DirectoryInfo languageDir = outputDirInfo.CreateSubdirectory(lang);
+                    CompileSingleLanguage(languageDir, lang);
+                }
+            }
+            else
+            {
+                CompileSingleLanguage(outputDirInfo);
+            }
+        }
+
+        private void CompileSingleLanguage(DirectoryInfo outputDirInfo, string selectedLanguage = null)
+        {
+            DirectoryInfo outputTemplateDirInfo = outputDirInfo.CreateSubdirectory(templateDirectory);
+            PrepareTemplateDirectory(templateDir, outputTemplateDirInfo, Data, selectedLanguage);
+            LatexCompiler.Compile(outputTemplateDirInfo.FullName, rootFile, auxDir: auxiliaryDirectory, outputDir: outputDirectory);
+            CleanUp(outputTemplateDirInfo);
+        }
+
+        private void CleanUp(DirectoryInfo outputTemplateDirInfo)
+        {
+            if(CleanUpAuxiliary)
+            {
+                DirectoryInfo auxDir = new DirectoryInfo(@$"{outputTemplateDirInfo.FullName}\{auxiliaryDirectory}");
+                auxDir.Delete(true);
+            }
+            if(CleanUpSyncTex)
+            {
+                DirectoryInfo outputDir = new DirectoryInfo(@$"{outputTemplateDirInfo.FullName}\{outputDirectory}");
+                string fileName = rootFile.Replace(".tex", ".synctex.gz");
+                FileInfo synctexfile = new FileInfo(@$"{outputDir.FullName}\{fileName}");
+                synctexfile.Delete();
+            }
+            if(CleanUpTemplate)
+            {
+                outputTemplateDirInfo.Delete(true);
+            }
         }
 
         private static void PrepareTemplateDirectory(DirectoryInfo templateDirInfo, DirectoryInfo outputDirInfo, TemplateData templateData, string selectedLanguage = null)
